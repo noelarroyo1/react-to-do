ddd1ca5d37db869adb41b6d65932ad547a997330
Noel Arroyo <noel.arroyo1@gmail.com>
2017-12-30 21:32:50 -0600

    add props and state features to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+       toDos: [
+         { description: 'Walk the Cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+   }
+
   render() {
     return (
       <div className="App">
        <ul>
-         <ToDo />
-         <ToDo />
+         { this.state.toDos.map( (toDo, index) =>
+           <ToDo key={ index } description={ toDo.description } isCompleted={ toDo.isCompleted } />
+         )}
        </ul>
       </div>
     );
